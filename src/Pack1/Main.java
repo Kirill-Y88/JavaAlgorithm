276c5715050999997b7a7692a2675e299b7deffb
Kirill <benberov09@mail.ru>
2020-11-22 17:29:27 +0300

    HW1

--- src/Pack1/Main.java
+++ src/Pack1/Main.java
@@ -3,6 +3,12 @@ package Pack1;
 public class Main {
     public static void main(String[] args) {
 
+    /*
+    Прочитано про Обольшое в книге Grokking Algorithms: An illustrated guide for programmers and other curious people.
+
+    Помимо GB прорешиваю задачи на JavaRush, несмотря на плохую подачу теории и большое количество бесполезных задач, там все же встречаются довольно полезные задачи(на мой взгляд)
+
+    */
 
 
 
