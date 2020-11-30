455cc73591a9e609805fab4b3946afb0bac2a4a0
Kirill <benberov09@mail.ru>
2020-11-30 20:52:02 +0300

    Lesson3 ДЗ 1)Реализован метод reverseString для возврата зеркальной строки с использованием стека 2)Реализован класс ДЭК

--- src/Pack1/Main.java
+++ src/Pack1/Main.java
@@ -4,46 +4,22 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-//        MyStack<Integer> stack = new MyStack<>();
-//        stack.push(1);
-//        stack.push(2);
-//        stack.push(3);
-//        stack.push(4);
-//
-//        System.out.println(stack.peek());
-//        for (int i = 0; i < 4; i++) {
-//            System.out.println(stack.pop());
-//        }
 
-//        Expression expression = new Expression("( 4-( 5+{ i*p } ) )");
-//        System.out.println(expression.checkBracket());
+        System.out.println(reverseString("мама мыла раму"));
 
 
-//        MyQueue<Integer> queue = new MyQueue<>(5);
-//        System.out.println(queue);
-//        queue.insert(5);
-//        System.out.println(queue);
-//        queue.insert(3);
-//        System.out.println(queue);
-//        queue.insert(2);
-//
-//        System.out.println(queue);
-//        System.out.println(queue.remove());
-//        System.out.println(queue);
-//        queue.insert(2);
-//        System.out.println(queue);
-//        queue.insert(2);
-//        System.out.println(queue);
-//        queue.insert(2);
+    }
 
-        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>(Comparator.reverseOrder());
-        mpq.insert(5);
-        mpq.insert(2);
-        mpq.insert(15);
-        mpq.insert(3);
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println(mpq.remove());
+    //Метод реверса строки с использованием стека
+    public static String reverseString(String inputString){
+        MyStack<Character> stack = new MyStack<>(inputString.length());
+        String outString = "";
+        for (int i = 0; i < inputString.length(); i++) {
+            stack.push(inputString.charAt(i));
         }
+        for (int i = 0; i < inputString.length(); i++) {
+            outString += stack.pop().toString();
+        }
+        return outString;
     }
 }
